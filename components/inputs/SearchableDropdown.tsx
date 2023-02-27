254838764c001a341117d000f598ac5f0a484374
Mike968 <michaelhmehall@gmail.com>
2023-02-27 13:29:00 -0700

    HELP

--- components/inputs/SearchableDropdown.tsx
+++ components/inputs/SearchableDropdown.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from "react";
+import EditDropdownTypeSheet from "../questionTypes/editable/EditDropdownFromSheet";
 
 export default function SearchableDropdown({
 	options,
@@ -29,7 +30,7 @@ export default function SearchableDropdown({
 
 	// FIX TYPE!!!!!!
 	function toggle(e: any) {
-		setIsOpen(e && e.target === inputRef.current);
+		// setIsOpen(e && e.target === inputRef.current);
 	}
 
 	const getDisplayValue = () => {
@@ -45,6 +46,14 @@ export default function SearchableDropdown({
 		);
 	};
 
+	const drop = (e: any) => {
+		if (e.target.value.length > 0) {
+			setIsOpen(true);
+		} else {
+			setIsOpen(false);
+		}
+	};
+
 	return (
 		<div className="w-full py-2">
 			<div className="">
@@ -56,6 +65,7 @@ export default function SearchableDropdown({
 					onChange={(e) => {
 						setQuery(e.target.value);
 						handleChange("");
+						drop(e);
 					}}
 					onClick={toggle}
 					placeholder={placeholder}
@@ -63,27 +73,29 @@ export default function SearchableDropdown({
 				/>
 			</div>
 
-			<div
-				className={`${
-					isOpen ? "absolute" : "hidden"
-				} max-h-48 w-80 overflow-auto rounded border-2 border-neutral-800 bg-neutral-50 text-gray-900`}
-			>
-				{filter(options).map((option: string, index: number) => {
-					return (
-						<div
-							onClick={() => selectOption(option)}
-							className={`${
-								option == selectedVal
-									? "bg-accent text-accent font-semibold"
-									: ""
-							} border-red hover:bg-accent hover:text-accent px-2 py-2 hover:font-semibold`}
-							key={index}
-						>
-							{option}
-						</div>
-					);
-				})}
-			</div>
+			{isOpen && (
+				<div
+					className={
+						"absolute max-h-48 w-80 overflow-auto rounded border-2 border-neutral-800 bg-neutral-50 text-gray-900"
+					}
+				>
+					{filter(options).map((option: string, index: number) => {
+						return (
+							<div
+								onClick={() => selectOption(option)}
+								className={`${
+									option == selectedVal
+										? "bg-gray-900 font-semibold text-neutral-50"
+										: ""
+								} px-2 py-2 hover:bg-gray-900 hover:font-semibold hover:text-neutral-50`}
+								key={index}
+							>
+								{option}
+							</div>
+						);
+					})}
+				</div>
+			)}
 		</div>
 	);
 }
--- pages/admin/whitelists.tsx
+++ pages/admin/whitelists.tsx
@@ -132,6 +132,27 @@ export default function Whitelists() {
 				<div>
 					<div className="mt-20 grid grid-cols-9 grid-rows-1">
 						<div className="col-span-5 col-start-3 flex w-full justify-between">
+							<Link
+								href="/admin"
+								className="font-xl group flex items-center rounded bg-neutral-50 px-2 font-bold text-gray-900 hover:bg-gray-900 hover:text-neutral-50"
+							>
+								<svg
+									aria-hidden="true"
+									fill="none"
+									stroke="currentColor"
+									strokeWidth={2}
+									viewBox="0 0 24 24"
+									xmlns="http://www.w3.org/2000/svg"
+									className="h-8 w-8"
+								>
+									<path
+										d="M15.75 19.5L8.25 12l7.5-7.5"
+										strokeLinecap="round"
+										strokeLinejoin="round"
+									/>
+								</svg>
+								Back
+							</Link>
 							<h1 className="text-4xl font-bold text-gray-900">
 								Whitelists
 							</h1>
@@ -231,7 +252,7 @@ export default function Whitelists() {
 							})}
 							{dataSnapshot.docs.length === 0 &&
 								!newWhitelist && (
-									<h4 className="font-light text-gray-500">
+									<h4 className="flex w-full justify-around font-light text-gray-500">
 										Make a new whitelist with the plus
 										button
 									</h4>
