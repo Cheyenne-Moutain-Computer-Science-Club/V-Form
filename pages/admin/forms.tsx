e1bcd42048cc693583d0f5fb3a4aefb0f1bd77da
Mike968 <michaelhmehall@gmail.com>
2023-03-06 13:23:33 -0700

    started

--- pages/admin/forms.tsx
+++ pages/admin/forms.tsx
@@ -15,6 +15,7 @@ import { Form } from "@/lib/types";
 import FormSplash from "@/components/creation-tools/FormSplash";
 import { useRouter } from "next/router";
 import { v4 as uuidv4 } from "uuid";
+import Link from "next/link";
 
 interface Poll {
 	header: string;
@@ -138,6 +139,27 @@ function Admin() {
 				<div>
 					<div className="mt-20 grid grid-cols-9 grid-rows-1">
 						<div className="col-span-5 col-start-3 flex w-full justify-between">
+							<Link
+								href="/admin"
+								className="font-xl group flex items-center rounded bg-neutral-50 px-2 font-bold text-gray-900 hover:bg-gray-900 hover:text-neutral-50"
+							>
+								<svg
+									aria-hidden="true"
+									fill="none"
+									stroke="currentColor"
+									strokeWidth={2}
+									viewBox="0 0 24 24"
+									xmlns="http://www.w3.org/2000/svg"
+									className="h-8 w-8"
+								>
+									<path
+										d="M15.75 19.5L8.25 12l7.5-7.5"
+										strokeLinecap="round"
+										strokeLinejoin="round"
+									/>
+								</svg>
+								Back
+							</Link>
 							<h1 className="text-4xl font-bold text-gray-900">
 								My Forms
 							</h1>
