11c258353b4ef5588869219fd2d8174df2fe3fc9
PiPinecone <58414056+PiPinecone@users.noreply.github.com>
2023-03-11 21:15:34 -0700

    fade fix

--- pages/admin/edit/[slug].tsx
+++ pages/admin/edit/[slug].tsx
@@ -286,6 +286,7 @@ export default function Edit() {
           title="Form saved -"
           text={"Last saved: " + new Date().toLocaleString()}
           color="green"
+          show={showAlert}
         />
       ) : null}
       <br className="my-7"/>
