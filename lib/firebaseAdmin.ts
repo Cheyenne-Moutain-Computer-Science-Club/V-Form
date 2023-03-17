f58b4ae5adb7ef9f0b8713f4f6e8c054c0257951
Mike968 <michaelhmehall@gmail.com>
2023-03-16 18:57:18 -0600

    Cloud Function Permission Fix

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -2,10 +2,11 @@ import * as admin from "firebase-admin";
 import { applicationDefault } from "firebase-admin/app";
 
 if (!admin.apps.length) {
-	admin.initializeApp({
-		credential: applicationDefault(),
-		// databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
-	});
+	admin.initializeApp();
+	// {
+	// credential: applicationDefault(),
+	// databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+	// }
 }
 
 export { admin };
